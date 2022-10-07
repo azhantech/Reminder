140b0762ffcf5d873d9f124c0ed5ac303ca6b18c
fashadSalsoft5632 <fashad.ahmed@salsoftnet.onmicrosoft.com>
2022-10-07 18:38:51 +0500

    android splash added

--- android/app/src/main/java/com/todoapp/MainActivity.java
+++ android/app/src/main/java/com/todoapp/MainActivity.java
@@ -1,5 +1,23 @@
 package com.todoapp;
 
+import static com.blitzapp.animatedsplash.animation.AnimatedText.FIT_XY;
+import static com.blitzapp.animatedsplash.animation.Constants.SCALE;
+import static com.blitzapp.animatedsplash.animation.Constants.SLIDE;
+import static com.blitzapp.animatedsplash.animation.Constants.SPLASH_SLIDE_DOWN;
+import static com.blitzapp.animatedsplash.animation.Constants.SPLASH_SLIDE_LEFT;
+import static com.blitzapp.animatedsplash.animation.Constants.SPLASH_FADE;
+import static com.blitzapp.animatedsplash.animation.Splash.screenHeight;
+import static com.blitzapp.animatedsplash.animation.Splash.screenWidth;
+
+import com.blitzapp.animatedsplash.animation.AnimatedObject;
+import com.blitzapp.animatedsplash.animation.GroupAnimation;
+import com.blitzapp.animatedsplash.animation.ObjectAnimation;
+import com.blitzapp.animatedsplash.animation.SingleAnimation;
+import com.blitzapp.animatedsplash.animation.Splash;
+
+import android.os.Bundle;
+import android.util.Log;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -7,7 +25,8 @@ import com.facebook.react.ReactRootView;
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -16,8 +35,10 @@ public class MainActivity extends ReactActivity {
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
-   * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView
+   * is created and
+   * you can specify the renderer you wish to use - the new renderer (Fabric) or
+   * the old renderer
    * (Paper).
    */
   @Override
@@ -40,9 +61,75 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected boolean isConcurrentRootEnabled() {
-      // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+      // If you opted-in for the New Architecture, we enable Concurrent Root (i.e.
+      // React 18).
       // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
       return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     }
+  }
+
+  public void onCreate(Bundle saved) {
+    super.onCreate(saved);
+    try {
+      initiateSplash();
+    } catch (Exception e) {
+      Log.e("ANIMATED_SPLASH", e.getMessage());
+    }
+  }
+
+  public void initiateSplash() {
+
+    // create dialog
+    Splash splash = new Splash(MainActivity.this);
+
+    // set background
+    splash.setBackgroundImage(R.drawable.splashbg);
+
+    // set splash hide animation
+    splash.setSplashHideAnimation(SPLASH_FADE);
+
+    // set splash hide delay
+    splash.setSplashHideDelay(1500);
+
+    // Create and add images to view
+    // AnimatedObject image1 = new AnimatedObject(R.drawable.header, screenHeight *
+    // 0.15, screenWidth);
+    // image1.setPositionX(0);
+    // image1.setPositionY(0);
+    // image1.setScaleType(FIT_XY);
+    // image1.setVisibility(false);
+    // splash.addAnimatedImage(image1);
+
+    // AnimatedObject image2 = new AnimatedObject(R.drawable.footer, screenHeight *
+    // 0.15, screenWidth, 0,
+    // screenHeight - screenHeight * 0.15, FIT_XY, false);
+    // image2.setPositionX(0);
+    // image2.setPositionY(screenHeight - screenHeight * 0.15f);
+    // image2.setScaleType(FIT_XY);
+    // image2.setVisibility(false);
+    // splash.addAnimatedImage(image2);
+
+    // AnimatedObject logoimage = new AnimatedObject(R.drawable.logo2, screenHeight
+    // * 0.18, screenWidth * 0.45);
+    // splash.addAnimatedImage(logoimage);
+    // ObjectAnimation image1Animation = new ObjectAnimation(image1, SLIDE, 780, 0f,
+    // 0f, -screenHeight * 0.15f, 0f);
+    // ObjectAnimation image2Animation = new ObjectAnimation(image2, SLIDE, 780, 0f,
+    // 0f, screenHeight * 0.15f, 0f);
+    // ObjectAnimation logoimageAnimation = new ObjectAnimation(logoimage, SCALE,
+    // 780, 0.2f, 1f, 0.2f, 1f);
+
+    // add group animation
+
+    // GroupAnimation group1 = new GroupAnimation(1);
+    // group1.addAnimation(image1Animation);
+    // group1.addAnimation(image2Animation);
+
+    // add single animation
+
+    // SingleAnimation singleAnimation = new SingleAnimation(logoimageAnimation, 2);
+
+    splash.ShowSplash();
+
   }
 }
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,15 +1,15 @@
 import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-// import AnimatedSplash from 'react-native-animated-splash';
+import AnimatedSplash from 'react-native-animated-splash';
 import AuthStack from './AuthStack';
 import MainStack from './MainStack';
 
 const RootNavigation = () => {
   // const isSignedIn = useSelector(state => state.auth.isLogedIn);
 
-  // useEffect(() => {
-  //   AnimatedSplash.hide();
-  // }, []);
+  useEffect(() => {
+    AnimatedSplash.hide();
+  }, []);
 
   return (
     <NavigationContainer>
