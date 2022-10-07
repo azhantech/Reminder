46eadeea71e3ba24d0ca20b86199ebf2480c1588
fashadSalsoft5632 <fashad.ahmed@salsoftnet.onmicrosoft.com>
2022-10-07 15:38:00 +0500

    Project Initialized

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import RootNavigation from './src/navigation/Root Navigation';
+
+const App = () => {
+  return (
+    <>
+      <RootNavigation />
+    </>
+  );
+};
+
+export default App;
