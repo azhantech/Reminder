66e580b82e20ee7fb870c866cd028ff98f38a43b
fashadSalsoft5632 <fashad.ahmed@salsoftnet.onmicrosoft.com>
2022-10-11 17:25:17 +0500

    tab navigation style fixed

--- src/util/CustomTabBarButton/styles.js
+++ src/util/CustomTabBarButton/styles.js
@@ -6,20 +6,20 @@ const styles = StyleSheet.create({
     top: -40,
     justifyContent: 'center',
     alignItems: 'center',
-    shadowColor: '#7F5DF0',
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.5,
-    elevation: 5,
   },
   containerStyle: {
     width: 70,
     height: 70,
     borderRadius: 35,
     backgroundColor: COLORS.mainFg,
+    shadowColor: '#7F5DF0',
+    shadowOffset: {
+      width: 0,
+      height: 20,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
   },
 });
 
