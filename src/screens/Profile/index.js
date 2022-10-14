44b0e32e389361f9b5e95018fdc50b4f4c9068ae
fashadSalsoft5632 <fashad.ahmed@salsoftnet.onmicrosoft.com>
2022-10-14 19:06:27 +0500

    Profile Page

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,11 +1,39 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {Text, View, Image, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
 import styles from './styles';
 
 const Profile = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <View style={styles.header}></View>
+      <Image
+        style={styles.avatar}
+        source={require('../../assets/icons/avatar.jpg')}
+      />
+      <View style={styles.body}>
+        <View style={styles.bodyContent}>
+          <Text style={styles.name}>username</Text>
+          <Text style={styles.info}>email</Text>
+
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.buttonContainer}
+            onPress={() => {
+              // dispatch(changeLogOut());
+              // dispatch(onLogout());
+
+              setTimeout(() => {
+                // navigation.navigate('Login');
+              }, 3000);
+            }}>
+            <Text style={styles.btnStyle}>Log Out</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -1,5 +1,73 @@
 import {StyleSheet} from 'react-native';
+import {COLORS, FONTS} from '../../constants';
 
-const styles = StyleSheet.create({});
-
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: COLORS.mainFg,
+    height: 200,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: COLORS.transparentWhite,
+    marginBottom: 10,
+    alignSelf: 'center',
+    position: 'absolute',
+    marginTop: 130,
+  },
+  name: {
+    fontSize: 22,
+    color: 'black',
+    fontWeight: '600',
+  },
+  body: {
+    marginTop: 40,
+  },
+  container: {
+    backgroundColor: COLORS.mainBg,
+    flex: 1,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 30,
+  },
+  name: {
+    fontSize: 28,
+    color: COLORS.white,
+    fontWeight: '600',
+    marginVertical: 30,
+    position: 'absolute',
+  },
+  info: {
+    fontSize: 16,
+    marginVertical: 70,
+    color: COLORS.white,
+    position: 'absolute',
+  },
+  description: {
+    fontSize: 16,
+    color: COLORS.white,
+    position: 'absolute',
+    textAlign: 'center',
+    marginVertical: 100,
+  },
+  buttonContainer: {
+    marginTop: 80,
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: COLORS.mainFg,
+  },
+  btnStyle: {
+    color: COLORS.white,
+    fontSize: FONTS.body3.fontSize,
+  },
+});
 export default styles;
