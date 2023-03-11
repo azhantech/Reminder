358fb4b552b883205c9a5a946b18eea2de593fc1
rizwandev294 <azhanhussainofficial@gmail.com>
2023-03-11 23:38:18 +0500

    Navigation

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+import {colors} from './src/utils/appTheme';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: colors.SpiritstoneRed}}></View>
+  );
+};
+
+export default App;
