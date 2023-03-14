bf1fc632add992f82fd3ebafd1aa0c56bb891e2c
syedumerali101 <syedumerali101@gmail.com>
2023-03-15 03:35:43 +0500

    Listing with switch button and component

--- src/Screen/AlarmListing/index.js
+++ src/Screen/AlarmListing/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Touchable, TouchableOpacity } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
+import SwitchComponent from '../../Component/SwitchComponent';
 import CircularBold from '../../Component/Texts/CircularBold';
 import { colors } from '../../utils/appTheme';
 import { vh, vw } from '../../utils/dimensions';
@@ -19,58 +20,8 @@ const AlarmList = ({ navigation }) => {
     { id: 10, name: '5:00 am', day: 'Mon, 13 Mar' },
   ];
   const renderItem = ({ item }) => {
-    console.log('Id =====================<', item);
     return (
-      <View
-        style={{
-          backgroundColor: '#fffA',
-          height: vh * 10,
-          width: vw * 90,
-          marginBottom: vh,
-          flexDirection: 'row',
-          alignItems: 'center',
-          elevation: 7,
-          borderRadius: vw * 2,
-        }}>
-        <View
-          style={{
-            height: '100%',
-            justifyContent: 'center',
-            width: '40%',
-            alignItems: 'center',
-          }}>
-          <CircularBold>{item?.name}</CircularBold>
-        </View>
-        <View
-          style={{
-            width: '60%',
-            height: '100%',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-around',
-          }}>
-          <View style={{ justifyContent: 'center' }}>
-            <CircularBold>{item?.day}</CircularBold>
-          </View>
-          <View
-            style={{
-              backgroundColor: 'grey',
-              height: vh * 2,
-              width: vw * 10,
-              borderRadius: vw * 4,
-              justifyContent: 'center',
-            }}>
-            <TouchableOpacity
-              style={{
-                backgroundColor: colors.white,
-                height: vh * 2,
-                width: vh * 2,
-
-                borderRadius: vw * 4,
-              }}></TouchableOpacity>
-          </View>
-        </View>
-      </View>
+      <SwitchComponent data={item} />
     );
   };
 
